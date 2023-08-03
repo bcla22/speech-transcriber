664b604fcf3cc5730ff3abdaee98bd23f4615703
Ben <22038077+bcla22@users.noreply.github.com>
2023-08-03 00:03:43 -0400

    add start-end times

--- transcriber.py
+++ transcriber.py
@@ -15,13 +15,14 @@ class Transcriber:
     def __exit__(self, exc_type, exc_value, traceback):
         self.model = None
 
-    def voice_to_text(self, audio_path: str, verbose: bool = False):
+    def voice_to_text(self, audio_path: str, verbose: bool = False, split_lines: bool = False):
         assert self._is_file(audio_path), f"Input file not found: {audio_path}"
         assert self._is_audio_file(audio_path), f"Input file is not an audio format: {audio_path}"
-        
         result = self.model.transcribe(audio_path, verbose=verbose, fp16=False)
+        if split_lines:
+            return "\n".join([segment['text'].strip() for segment in result['segments']])
         return result["text"]
-
+    
     def write_output_to_disk(self, text: str, output_path: str):
         if self._is_file(output_path):
             output_path = self._add_number_to_filename(output_path)
